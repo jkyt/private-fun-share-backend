27eb25630d4c75249e11c3b8de039b39728f14bf
yuantao <469622850@qq.com>
2023-08-07 10:44:09 +0800

    master

--- sfx-framework/src/main/java/com/funshare/framework/aspectj/DataSourceAspect.java
+++ sfx-framework/src/main/java/com/funshare/framework/aspectj/DataSourceAspect.java
@@ -27,8 +27,8 @@ public class DataSourceAspect
 {
     protected Logger logger = LoggerFactory.getLogger(getClass());
 
-    @Pointcut("@annotation(com.ruoyi.common.annotation.DataSource)"
-            + "|| @within(com.ruoyi.common.annotation.DataSource)")
+    @Pointcut("@annotation(com.funshare.common.annotation.DataSource)"
+            + "|| @within(com.funshare.common.annotation.DataSource)")
     public void dsPointCut()
     {
 
